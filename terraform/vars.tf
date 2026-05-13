5e318d832fc76d3d885b1bf1766dd71ffa797c41
Gdope <snedeljkovic2003@gmail.com>
2026-05-13 18:26:56 +0200

    Added VPC

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -5,3 +5,35 @@ variable "region" {
 variable "aws_key_pair_name" {
   default = "stefan-intens-key"
 }
+
+variable "vpc_name" {
+  default = "stefan-intens-vpc"
+}
+
+variable "VPC_CIDR" {
+  default = "172.20.0.0/16"
+}
+
+variable "zone1" {
+  default = "us-east-1a"
+}
+
+variable "zone2" {
+  default = "us-east-1b"
+}
+
+variable "zone3" {
+  default = "us-east-1c"
+}
+
+variable "pub_sub1" {
+  default = "172.20.10.0/24"
+}
+
+variable "pub_sub2" {
+  default = "172.20.11.0/24"
+}
+
+variable "pub_sub3" {
+  default = "172.20.12.0/24"
+}
