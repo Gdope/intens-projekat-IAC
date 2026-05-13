838e1f5680434c74f2969ae5c23f5571a53505b9
Gdope <snedeljkovic2003@gmail.com>
2026-05-13 18:28:30 +0200

    Terraform fmt for vpc.tf fix

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,13 +4,13 @@ module "vpc" {
   name = var.vpc_name
   cidr = var.VPC_CIDR
 
-  azs             = [var.zone1, var.zone2, var.zone3]
-  public_subnets  = [var.pub_sub1, var.pub_sub2, var.pub_sub3]
+  azs            = [var.zone1, var.zone2, var.zone3]
+  public_subnets = [var.pub_sub1, var.pub_sub2, var.pub_sub3]
 
 
-  enable_dns_hostnames = true
+  enable_dns_hostnames    = true
   map_public_ip_on_launch = true
-  enable_dns_support = true
+  enable_dns_support      = true
 
   tags = {
     project = "intens-project"
