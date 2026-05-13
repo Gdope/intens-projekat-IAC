fc1895895e59b4acf86a4ee1deca0d1492c6f2d8
Gdope <snedeljkovic2003@gmail.com>
2026-05-13 18:50:09 +0200

    Terraform fmt for secgrp.tf

--- terraform/secgrp.tf
+++ terraform/secgrp.tf
@@ -27,12 +27,12 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_alb_ipv6" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_alb" {
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
+  ip_protocol       = "-1"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6_alb" {
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" 
+  ip_protocol       = "-1"
 }
 
