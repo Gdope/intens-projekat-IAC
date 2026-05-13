7c5bc5b6e1bb7a9bbc5b1afe095a0e20c0e018d7
Gdope <snedeljkovic2003@gmail.com>
2026-05-13 18:58:33 +0200

    Updated ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,6 +1,11 @@
 resource "aws_ecs_cluster" "intens_container" {
   name = var.ecs_name
 
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
   tags = {
     project = "intens-project"
   }
